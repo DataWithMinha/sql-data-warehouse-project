21adc5042fa015230e68e92afb03569237014cbf
DataWithMinha <minhakhan8954@gmail.com>
2026-07-29 19:43:24 +0530

    Update load_bronze.sql

--- scripts/Bronze/load_bronze.sql
+++ scripts/Bronze/load_bronze.sql
@@ -16,6 +16,7 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
@@ -35,7 +36,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+		FROM 'C:\Users\minha\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm/cust_info.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -50,7 +51,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_prd_info;
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+		FROM 'C:\Users\minha\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm/prd_info.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -65,7 +66,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+		FROM 'C:\Users\minha\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm/sales_details.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -84,7 +85,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+		FROM 'C:\Users\minha\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp/LOC_A101.csv'
 		WITH (
 					FIRSTROW = 2,
 					FIELDTERMINATOR = ',',
@@ -99,7 +100,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+		FROM 'C:\Users\minha\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp/CUST_AZ12.csv'
 		WITH (
 					FIRSTROW = 2,
 					FIELDTERMINATOR = ',',
@@ -114,7 +115,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+		FROM 'C:\Users\minha\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp/PX_CAT_G1V2.csv'
 		WITH (
 					FIRSTROW = 2,
 					FIELDTERMINATOR = ',',
@@ -134,4 +135,3 @@ BEGIN
 		PRINT '=========================================='
 	END CATCH
 END
-
